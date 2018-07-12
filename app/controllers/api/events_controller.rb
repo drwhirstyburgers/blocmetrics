aaebd08d2aecd11a55baf3d19321f7b28f5f1848
Drwhirstyburgers <dave.hirst50@gmail.com>
2018-07-12 15:54:42 -0400

    API CORS requests working

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,7 +1,15 @@
 class API::EventsController < ApplicationController
+  # skip_before_action :verify_authenticity_token
+  before_action :set_access_control_headers
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Content-Type'
+  end
 
   def create
-    registered_application = RegisteredApplication.find_by(url: request.env['HTTP_ORIGIN'])
+    registered_application = RegisteredApplication.find_by(registration: request.env['HTTP_ORIGIN'])
 
     if registered_application.nil?
       render json: "Unregistered application", status: :unprocessable_entity
@@ -12,8 +20,12 @@ class API::EventsController < ApplicationController
         else
           render json: { error: "missing event name" }, status: :unprocessable_entity
         end
-      end
     end
+  end
+
+  def preflight
+    head 200
+  end
 
   private
 
